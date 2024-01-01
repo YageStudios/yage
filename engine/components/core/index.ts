495b97b485886278c7bb9a1aba0dfd6d222de7f9
YageStudios <YageStudios@gmail.com>
2024-01-01 18:23:47 -0500

    owned owners

--- engine/components/core/index.ts
+++ engine/components/core/index.ts
@@ -3,3 +3,5 @@ export * from "./FrameRate";
 export * from "./Random";
 export * from "./SpatialMap";
 export * from "../../schemas/core/PlayerInput";
+export * from "./Owner";
+export * from "./Owned";
