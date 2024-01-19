9a8e0f6e4fc5e1bd9615953294b4b2509ca83e8d
YageStudios <YageStudios@gmail.com>
2024-01-19 13:14:29 -0500

    optional window scoped hacks

---
+++ engine/utils/windowSessionStorage.ts
@@ -0,0 +1,23 @@
+export var windowSessionStorage = (function () {
+  // Use window.name as the unique identifier for the current window
+  if (!window.name) {
+    window.name = Math.random().toString(36).substring(2, 11);
+  }
+
+  return {
+    setItem: function (key: string, value: string) {
+      sessionStorage.setItem(window.name + key, value);
+    },
+    getItem: function (key: string) {
+      return sessionStorage.getItem(window.name + key);
+    },
+    removeItem: function (key: string) {
+      sessionStorage.removeItem(window.name + key);
+    },
+    clear: function () {
+      Object.keys(sessionStorage)
+        .filter((k) => k.startsWith(window.name))
+        .forEach((k) => sessionStorage.removeItem(k));
+    },
+  };
+})();
