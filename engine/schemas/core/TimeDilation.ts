3ef001102c6155aeb185c689886f1c3e3f76499e
Yage Studios <yagestudios@gmail.com>
2024-10-06 06:05:02 -0400

    remove timedilation

--- engine/schemas/core/TimeDilation.ts
+++
@@ -1,8 +0,0 @@
-import { Component, defaultValue, Schema, type } from "minecs";
-
-@Component()
-export class TimeDilation extends Schema {
-  @type("number")
-  @defaultValue(0.5)
-  amount: number;
-}
