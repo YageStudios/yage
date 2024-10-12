9a73594be296c004338aede5f22c02a2781ac6cd
YageStudios <YageStudios@gmail.com>
2024-10-12 10:28:55 -0400

    add cross product

--- engine/utils/vector.ts
+++ engine/utils/vector.ts
@@ -86,16 +86,16 @@ export const perpendicularVector2d = (v: Vector2d): Vector2d => {
   return { x: -v.y, y: v.x };
 };
 
-export const equalVector2d = (a: Vector2d | Vector2d, b: Vector2d | Vector2d) => a.x === b.x && a.y === b.y;
+export const equalVector2d = (a: Vector2d, b: Vector2d) => a.x === b.x && a.y === b.y;
 
-export const truncVector2d = (a: Vector2d | Vector2d, trunc = 10000) => {
+export const truncVector2d = (a: Vector2d, trunc = 10000) => {
   return {
     x: Math.floor(a.x * trunc) / trunc,
     y: Math.floor(a.y * trunc) / trunc,
   };
 };
 
-export const clamp = (a: Vector2d | Vector2d, trunc = 10000): void => {
+export const clamp = (a: Vector2d, trunc = 10000): void => {
   a.x = Math.floor(a.x * trunc) / trunc;
   a.y = Math.floor(a.y * trunc) / trunc;
 };
@@ -109,26 +109,22 @@ export const trunc2d = (x: number, y: number, trunc = 10000) => {
 
 export const lerpScalar = (a: number, b: number, t: number) => a + (b - a) * t;
 
-export const lerpVector2d = (a: Vector2d | Vector2d, b: Vector2d | Vector2d, t: number) => {
+export const lerpVector2d = (a: Vector2d, b: Vector2d, t: number) => {
   return {
     x: lerpScalar(a.x, b.x, t),
     y: lerpScalar(a.y, b.y, t),
   };
 };
 
-export const distanceSquaredVector2d = (a: Vector2d | Vector2d, b: Vector2d | Vector2d) => {
+export const distanceSquaredVector2d = (a: Vector2d, b: Vector2d) => {
   return (a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y);
 };
 
-export const distanceVector2d = (a: Vector2d | Vector2d, b: Vector2d | Vector2d): number => {
+export const distanceVector2d = (a: Vector2d, b: Vector2d): number => {
   return Math.sqrt(distanceSquaredVector2d(a, b));
 };
 
-export const inDistanceVector2d = (
-  origin: Vector2d | Vector2d,
-  search: Vector2d | Vector2d,
-  distance: number
-): boolean => {
+export const inDistanceVector2d = (origin: Vector2d, search: Vector2d, distance: number): boolean => {
   return distanceSquaredVector2d(origin, search) <= distance * distance;
 };
 
@@ -136,7 +132,7 @@ export const inDistanceFastVector2d = (ox: number, oy: number, sx: number, sy: n
   return (sx - ox) * (sx - ox) + (sy - oy) * (sy - oy) <= distance * distance;
 };
 
-export const normalizeOrRandomizeVector2d = (vector: Vector2d | Vector2d, rand: Random): Vector2d => {
+export const normalizeOrRandomizeVector2d = (vector: Vector2d, rand: Random): Vector2d => {
   if (vector.x === 0 && vector.y === 0) {
     const random = rand.int(0, 100) / 100;
     return normalizeVector2d(w({ x: random - 0.5, y: 1 - random - 0.5 }));
@@ -144,55 +140,55 @@ export const normalizeOrRandomizeVector2d = (vector: Vector2d | Vector2d, rand: 
   return normalizeVector2d(vector);
 };
 
-export const lengthVector2d = (vector: Vector2d | Vector2d): number => {
+export const lengthVector2d = (vector: Vector2d): number => {
   return distanceVector2d(w({ x: 0, y: 0 }), vector);
 };
 
-export const rotationDegVector2d = (vector: Vector2d | Vector2d): number => {
+export const rotationDegVector2d = (vector: Vector2d): number => {
   return (rotationVector2d(vector) * 180) / Math.PI;
 };
 
-export const rotationVector2d = (vector: Vector2d | Vector2d): number => {
+export const rotationVector2d = (vector: Vector2d): number => {
   return Math.atan2(vector.y, vector.x);
 };
 
-export const normalizeSafeVector2d = (vector: Vector2d | Vector2d): Vector2d => {
+export const normalizeSafeVector2d = (vector: Vector2d): Vector2d => {
   if (vector.x === 0 && vector.y === 0) {
     return w({ x: 0, y: 0 });
   }
   return normalizeVector2d(vector);
 };
 
-export const normalizeVector2d = (a: Vector2d | Vector2d): Vector2d => {
+export const normalizeVector2d = (a: Vector2d): Vector2d => {
   const length = lengthVector2d(a);
   return w(trunc2d(a.x / length, a.y / length));
 };
 
-export const subtractVector2d = (a: Vector2d | Vector2d, b: Vector2d | Vector2d): Vector2d => {
+export const subtractVector2d = (a: Vector2d, b: Vector2d): Vector2d => {
   return w({
     x: a.x - b.x,
     y: a.y - b.y,
   });
 };
 
-export const addVector2d = (a: Vector2d | Vector2d, b: Vector2d | Vector2d, c?: Vector2d | Vector2d): Vector2d => {
+export const addVector2d = (a: Vector2d, b: Vector2d, c?: Vector2d): Vector2d => {
   return w({
     x: a.x + b.x + (c?.x ?? 0),
     y: a.y + b.y + (c?.y ?? 0),
   });
 };
-export const scaleVector2d = (a: Vector2d | Vector2d, scale: number): Vector2d => {
+export const scaleVector2d = (a: Vector2d, scale: number): Vector2d => {
   return w({
     x: a.x * scale,
     y: a.y * scale,
   });
 };
 
-export const angleOfVector2d = (a: Vector2d | Vector2d): number => {
+export const angleOfVector2d = (a: Vector2d): number => {
   return angleBetweenVector2d(w({ x: 1, y: 0 }), a);
 };
 
-export const angleBetweenVector2d = (a: Vector2d | Vector2d, b: Vector2d | Vector2d): number => {
+export const angleBetweenVector2d = (a: Vector2d, b: Vector2d): number => {
   let angle = Math.atan2(b.y, b.x) - Math.atan2(a.y, a.x);
   if (angle < 0) {
     angle += 2 * Math.PI;
@@ -204,7 +200,7 @@ export const degreesFromVector2d = (a: Vector2d): number => {
   return (Math.atan2(a.y, a.x) * 180) / Math.PI;
 };
 
-export const rotateDegVector2d = (a: Vector2d | Vector2d, degrees: number): Vector2d => {
+export const rotateDegVector2d = (a: Vector2d, degrees: number): Vector2d => {
   if (degrees % 360 === 0) {
     return w({
       ...a,
@@ -214,30 +210,31 @@ export const rotateDegVector2d = (a: Vector2d | Vector2d, degrees: number): Vect
   return rotateVector2d(a, rad);
 };
 
-export const multiplyVector2d = (a: Vector2d | Vector2d, b: Vector2d | Vector2d): Vector2d =>
+export const multiplyVector2d = (a: Vector2d, b: Vector2d): Vector2d =>
   w({
     x: a.x * b.x,
     y: a.y * b.y,
   });
 
-export const dotVector2d = (a: Vector2d | Vector2d, b: Vector2d | Vector2d): number => {
+export const dotVector2d = (a: Vector2d, b: Vector2d): number => {
   return a.x * b.x + a.y * b.y;
 };
 
-export const rotateVector2d = (a: Vector2d | Vector2d, radians: number): Vector2d => {
+export const crossProductVector2d = (v1: Vector2d, v2: Vector2d): number => {
+  return v1.x * v2.y - v1.y * v2.x;
+};
+
+export const rotateVector2d = (a: Vector2d, radians: number): Vector2d => {
   const cos = Math.cos(radians);
   const sin = Math.sin(radians);
   return w(trunc2d(a.x * cos - a.y * sin, a.x * sin + a.y * cos));
 };
 
-export const pointingTowardsVector2d = (a: Vector2d | Vector2d, b: Vector2d | Vector2d): boolean => {
+export const pointingTowardsVector2d = (a: Vector2d, b: Vector2d): boolean => {
   return dotVector2d(a, b) > 0;
 };
 
-export const pointTowardsVector2d = (
-  origin: Vector2d | Vector2d,
-  target: (Vector2d | Vector2d)[] | Vector2d | Vector2d
-): Vector2d => {
+export const pointTowardsVector2d = (origin: Vector2d, target: Vector2d[] | Vector2d): Vector2d => {
   let dir = w({ x: 0, y: 0 });
   if (!Array.isArray(target)) {
     dir = subtractVector2d(target, origin);
@@ -253,7 +250,7 @@ export const pointTowardsVector2d = (
   return dir;
 };
 
-export const seekTowardsVector2d = (origin: Vector2d | Vector2d, speed = 1, target: Vector2d | Vector2d) => {
+export const seekTowardsVector2d = (origin: Vector2d, speed = 1, target: Vector2d) => {
   if (equalVector2d(target, origin)) {
     return origin;
   }
@@ -261,11 +258,7 @@ export const seekTowardsVector2d = (origin: Vector2d | Vector2d, speed = 1, targ
   return addVector2d(origin, speed > 1 ? scaleVector2d(pointVector, speed) : pointVector);
 };
 
-export const avoidVector2d = (
-  origin: Vector2d | Vector2d,
-  speed = 1,
-  target: (Vector2d | Vector2d)[] | Vector2d | Vector2d
-) => {
+export const avoidVector2d = (origin: Vector2d, speed = 1, target: Vector2d[] | Vector2d) => {
   if (Array.isArray(target) && target.length === 0) {
     return origin;
   }
@@ -273,21 +266,13 @@ export const avoidVector2d = (
   return addVector2d(origin, scaleVector2d(pointVector, -speed));
 };
 
-export const isCloserVector2d = (
-  origin: Vector2d | Vector2d,
-  a: Vector2d | Vector2d,
-  b: Vector2d | Vector2d
-): boolean => {
+export const isCloserVector2d = (origin: Vector2d, a: Vector2d, b: Vector2d): boolean => {
   const distanceA = distanceVector2d(origin, a);
   const distanceB = distanceVector2d(origin, b);
   return distanceA < distanceB;
 };
 
-export const RayBoxIntersection = (
-  origin: Vector2d | Vector2d,
-  direction: Vector2d | Vector2d,
-  box: Box2d
-): boolean => {
+export const RayBoxIntersection = (origin: Vector2d, direction: Vector2d, box: Box2d): boolean => {
   const t1 = (box.x - origin.x) / direction.x;
   const t2 = (box.x + box.width - origin.x) / direction.x;
   const t3 = (box.y - origin.y) / direction.y;
@@ -299,11 +284,7 @@ export const RayBoxIntersection = (
   return tmax >= tmin && tmax >= 0;
 };
 
-export const RayBoxIntersectionPoint = (
-  origin: Vector2d | Vector2d,
-  direction: Vector2d | Vector2d,
-  box: Box2d
-): Vector2d => {
+export const RayBoxIntersectionPoint = (origin: Vector2d, direction: Vector2d, box: Box2d): Vector2d => {
   const t1 = (box.x - origin.x) / direction.x;
   const t2 = (box.x + box.width - origin.x) / direction.x;
   const t3 = (box.y - origin.y) / direction.y;
